91dd22d28c447985b56e83860827dd4f3843094c
Leonardo Oliveira <leonardo@ipeweb.com.br>
2022-12-25 17:16:13 -0300

    vercel

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
-var cors = require('cors');
+import cors from 'cors';
 
 import { router } from './routes';
 import documentacaoFile from './swagger.json';
@@ -8,14 +8,16 @@ import documentacaoFile from './swagger.json';
 const app = express();
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", `*`);
-    res.header("Access-Control-Allow-Headers", `*`);
-    app.use(cors())
-    next();
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', '*');
+	app.use(cors());
+	next();
 });
 
 app.use(express.json());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(documentacaoFile));
 app.use(router);
 
-app.listen(8000, () => console.log('Servidor Online'));
+const PORT = 8000;
+
+app.listen(PORT, () => console.log(`Servidor Online ON PORT ${PORT}`));
