24818e71d804310b05349cd5deef91539039b93c
Leonardo Oliveira <leonardo@ipeweb.com.br>
2022-12-26 09:57:56 -0300

    cors

--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 
 import { router } from './src/routes';
 import documentacaoFile from './src/swagger.json';
 
 const app = express();
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', '*');
+	app.use(cors());
+	next();
+});
 
 app.use(express.json());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(documentacaoFile));
